ebb0e2db583c1120664062faf4008c798f784ef1
shoshi4269 <sh6704269@gmail.com>
2021-12-26 18:25:37 +0200

    addButtonComponentForPayment

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+export default function Buttonn  () {
+    return (
+            <Button variant="outlined" startIcon={<ArrowBackIosNewIcon />} color="secondary">
+                המשך לתשלום
+            </Button>
+    )
+}
