a08b3014f0822b9c4450b6bf8c6f91d09ee6ac19
racheliSHu <rachel33052@gmail.com>
2021-12-26 17:43:24 +0200

    submit

---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+
+const BootstrapButton = styled(Button)({
+    fontSize:17,margin:'auto',padding: '6px 12px'
+});
+export default function CustomizedButtons() {
+    return (
+      <Stack spacing={2} direction="column" >
+        <BootstrapButton variant="contained" disableRipple>
+        אשר הכנסת סכום מדויק לתשלום בקופות הייעודיות
+        </BootstrapButton>
+        👇
+      </Stack>
+       
+    );
+  }
