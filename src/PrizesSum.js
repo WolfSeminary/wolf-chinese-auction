c9dccca8028bf0650a589d2209c8217bcab6285f
rivka-fruchter <r0556779798@gmail.com>
2021-12-26 17:13:13 +0200

    AddSumPrizesWithIcon

---
+++ src/PrizesSum.js
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import Badge from '@mui/material/Badge';
+
+const Gifticon=()=>{
+    return <img src="https://img.icons8.com/ios-glyphs/30/000000/gift--v1.png"/>
+}
+const PrizesSum=()=>{
+ return  ( <Badge badgeContent={2} color="primary">
+<Gifticon />
+</Badge>
+)
+}
+export default PrizesSum
