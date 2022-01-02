63735f351ef945f4098ba3c6113ed8b59bb2eaf3
hitmachut <hitmachut@students.local>
2022-01-02 17:40:26 +0200

    sa git commit -m sa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import PricesTable from './PricesTable '
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <PricesTable/>
       </header>
     </div>
   );
