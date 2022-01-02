2ca4109161a91c96e77588dc5d3014fbbdb5f3f5
yeuhditK <yesitsme5084@gmail.com>
2022-01-02 18:51:29 +0200

    a grid of Prizes

---
+++ src/Prizes.js
@@ -0,0 +1,56 @@
+import  React,  {useState} from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+const Prizes = () => {
+    const [prizes, setPrizes] = useState(
+        [
+            {
+                ID: 1,
+                Name: "חדר ילדים",
+                // image:<Image URL here></Image>,
+                Description: "ארון עץ, מיטת נוער, שולחן",
+                Price: 30
+            },
+            {
+                ID: 2,
+                Name: "חדר ילדים",
+                // image:<Image URL here></Image>,
+                Description: "ארון עץ, מיטת נוער, שולחן",
+                Price: 40
+            },
+            {
+                ID: 3,
+                Name: "חדר ילדים",
+                // image:<Image URL here></Image>,
+                Description: "ארון עץ, מיטת נוער, שולחן",
+                Price: 20
+            },
+            {
+                ID: 4,
+                Name: "חדר ילדים",
+                // image:<Image URL here></Image>,
+                Description: "ארון עץ, מיטת נוער, שולחן",
+                Price: 50
+            }
+
+        ]
+    )
+    return (<>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 4, md: 8 }}>
+                {prizes.map((item, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <div>
+                            <p> Add Prize  component here</p>
+                            <p>{item.ID}</p>
+                            <p>{item.Name}</p>
+                            <p> {item.Description}</p>
+                        </div>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    </>);
+}
+
+export default Prizes;
