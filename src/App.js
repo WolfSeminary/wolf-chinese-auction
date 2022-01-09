25e5878ad7f50a7cdf6e238158bb43ddf1be3b34
shoshi4269 <sh6704269@gmail.com>
2022-01-09 14:00:26 +0200

    feat(prizesPage):put together components in one page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-//import logo from './logo.svg';
 import './App.css';
-import PaymentBtn from './Button';
 function App() {
   return (
     <div className="App">
-      <PaymentBtn/>
+     
     </div>
   );
 }
---
+++ src/PrizesPage.js
@@ -0,0 +1,10 @@
+export default function PrizesPage() {
+    return (
+        <>
+            <AppBar/>
+            <Prizes/>
+            <ContinueToPayment/>
+            <NoticeModal/>
+        </>
+    )
+}
