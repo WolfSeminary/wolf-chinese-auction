4a2f04ae0bf42dea7de7f621ad0e77e20bc8fdad
hitmachut <hitmachut@students.local>
2022-01-16 19:11:27 +0200

    FixDirectionForm

--- src/ThankYouModal.js
+++ src/ThankYouModal.js
@@ -2,26 +2,35 @@ import React from "react";
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
 export default () => {
+    const cacheRtl = createCache({
+        key: 'muirtl',
+        stylisPlugins: [rtlPlugin],
+    });
     return (
         <>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 על מנת לשמור את נתוניך לצורך עריכת ההגרלות, נא הזן את הפרטים הבאים
             </Typography>
-            <div dir="rtl">
-                <Box
-                    component="form"
-                    sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField id="outlined-basic" label="שם פרטי" variant="outlined" />
-                    <TextField id="outlined-basic" label="שם משפחה" variant="outlined" />
-                </Box>
-            </div>
+            <CacheProvider value={cacheRtl}>
+                <div dir="rtl">
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <TextField id="outlined-basic" label="שם פרטי" variant="outlined" />
+                        <TextField id="outlined-basic" label="שם משפחה" variant="outlined" />
+                    </Box>
+                </div>
+            </CacheProvider>
         </>
     )
 }
