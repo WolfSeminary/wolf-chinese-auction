6d1757c4da1fdd7a600c13f42d4cf979e7094b36
shoshi4269 <sh6704269@gmail.com>
2022-01-16 16:20:54 +0200

    feat(prizesState):add images to prizes array

--- src/statePrizes.js
+++ src/statePrizes.js
@@ -1,90 +1,94 @@
 import { useState } from "react";
-const [prizes, setPrizes] = useState();
-const prizesInfoArray = [
 
-    {
-        "PrizeID": "1",
-        "prizeName": "חדר ילדים",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "ארון, מיטה, שידה",
-        "prizePrice": "20"
-    },
-    {
-        "PrizeID": "2",
-        "prizeName": "פאה",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "פאת קסטם",
-        "prizePrice": "20"
-    },
-    {
-        "PrizeID": "3",
-        "prizeName": "מייבש כביסה",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": " מייבש כביסה של חברת ZANUSSI",
-        "prizePrice": "20"
-    },
-    {
-        "PrizeID": "4",
-        "prizeName": "עגלת תינוק",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "עגלת שכיבה לתינוק",
-        "prizePrice": "20"
-    },
-    {
-        "PrizeID": "5",
-        "prizeName": "נעליים לכל המשפחה",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "10 זוגות נעליים לכל המשפחה",
-        "prizePrice": "10"
-    },
-    {
-        "PrizeID": "6",
-        "prizeName": "1000₪ ביש",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "שובר כספי לרשת יש",
-        "prizePrice": "10"
-    },
-    {
-        "PrizeID": "7",
-        "prizeName": "1000₪ בקידישיק",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "שובר כספי לרשת קידישיק",
-        "prizePrice": "10"
-    },
-    {
-        "PrizeID": "8",
-        "prizeName": "1000₪ בבת של מלך",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "שובר כספי לרשת בת מלך",
-        "prizePrice": "10"
-    },
-    {
-        "PrizeID": "9",
-        "prizeName": "ערכת פליימוביל",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "בית בובות ענק",
-        "prizePrice": "5"
-    },
-    {
-        "PrizeID": "10",
-        "prizeName": "סט מצעים",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "זוג מצעים מפואר",
-        "prizePrice": "5"
-    },
-    {
-        "PrizeID": "11",
-        "prizeName": "אורגית",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "אורגנית איכותית",
-        "prizePrice": "5"
-    },
-    {
-        "PrizeID": "12",
-        "prizeName": "מחשב נייד",
-        "prizeImage": "<Image URL here>",
-        "prizeDescription": "מחשב נייד קומפקטי",
-        "prizePrice": "5"
-    }
-]
-setPrizes(prizes = prizesInfoArray);
+const Prizes = () => {
+    const [prizes, setPrizes] = useState();
+    const prizesInfoArray = [
+
+        {
+            "PrizeID": "1",
+            "prizeName": "חדר ילדים",
+            "prizeImage": "../../images/childrenRoom",
+            "prizeDescription": "ארון, מיטה, שידה",
+            "prizePrice": "20"
+        },
+        {
+            "PrizeID": "2",
+            "prizeName": "פאה",
+            "prizeImage": "<Image URL here>",
+            "prizeDescription": "פאת קסטם",
+            "prizePrice": "20"
+        },
+        {
+            "PrizeID": "3",
+            "prizeName": "מייבש כביסה",
+            "prizeImage": "../../images/dryer",
+            "prizeDescription": " מייבש כביסה של חברת ZANUSSI",
+            "prizePrice": "20"
+        },
+        {
+            "PrizeID": "4",
+            "prizeName": "עגלת תינוק",
+            "prizeImage": "../../images/babyStroller",
+            "prizeDescription": "עגלת שכיבה לתינוק",
+            "prizePrice": "20"
+        },
+        {
+            "PrizeID": "5",
+            "prizeName": "נעליים לכל המשפחה",
+            "prizeImage": "<Image URL here>",
+            "prizeDescription": "10 זוגות נעליים לכל המשפחה",
+            "prizePrice": "10"
+        },
+        {
+            "PrizeID": "6",
+            "prizeName": "1000₪ ביש",
+            "prizeImage": "../../images/yesh",
+            "prizeDescription": "שובר כספי לרשת יש",
+            "prizePrice": "10"
+        },
+        {
+            "PrizeID": "7",
+            "prizeName": "1000₪ בקידישיק",
+            "prizeImage": "../../images/kidishik",
+            "prizeDescription": "שובר כספי לרשת קידישיק",
+            "prizePrice": "10"
+        },
+        {
+            "PrizeID": "8",
+            "prizeName": "1000₪ בבת של מלך",
+            "prizeImage": "../../images/batMelech",
+            "prizeDescription": "שובר כספי לרשת בת מלך",
+            "prizePrice": "10"
+        },
+        {
+            "PrizeID": "9",
+            "prizeName": "ערכת פליימוביל",
+            "prizeImage": "../../images/dollsHouse",
+            "prizeDescription": "בית בובות ענק",
+            "prizePrice": "5"
+        },
+        {
+            "PrizeID": "10",
+            "prizeName": "סט מצעים",
+            "prizeImage": "../../images/matzaim",
+            "prizeDescription": "זוג מצעים מפואר",
+            "prizePrice": "5"
+        },
+        {
+            "PrizeID": "11",
+            "prizeName": "אורגית",
+            "prizeImage": "../../images/organit",
+            "prizeDescription": "אורגנית איכותית",
+            "prizePrice": "5"
+        },
+        {
+            "PrizeID": "12",
+            "prizeName": "מחשב נייד",
+            "prizeImage": "../../images/laptop",
+            "prizeDescription": "מחשב נייד קומפקטי",
+            "prizePrice": "5"
+        }
+    ]
+    setPrizes(prizes = prizesInfoArray)
+}
+export default Prizes;
