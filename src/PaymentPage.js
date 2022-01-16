47fb68f9e4de647eb73ccc495a19e894068d782d
racheliSHu <rachel33052@gmail.com>
2022-01-16 19:07:51 +0200

    router

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from "react-router-dom";
 
 export default function ButtonBackToGifts() {
+  let navigate = useNavigate();
   return (
-   <Button variant="contained" disableElevation sx={{
+
+    <Button variant="contained" disableElevation sx={{
       position: 'fixed',
       m: 0,
       top: 10,
       right: 10
-    }} >
+    }}
+      onClick={() =>
+        navigate(`/prize_page`)}
+    >
       <ArrowBackIcon />בחזרה לבחירת המתנות
+
     </Button>
   );
 }
