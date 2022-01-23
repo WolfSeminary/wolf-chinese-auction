d68554674c95b9c6ba6ff6038c8f8ad38afdf560
racheliSHu <rachel33052@gmail.com>
2022-01-23 15:09:12 +0200

    model

--- src/PrizesPage.js
+++ src/PrizesPage.js
@@ -10,7 +10,7 @@ export default function PrizesPage() {
             <AppBar />
             <Prizes />
             <ContinueToPayment />
-            {shouldShowNoticeModal && <NoticeModal />} 
+            {shouldShowNoticeModal && <NoticeModal />}
         </>
     );
 }
