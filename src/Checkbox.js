0197bbfce81471b9233b55927ea129971fd6d153
chayale5281 <chayale5281@gmauk.com>
2022-01-23 15:40:23 +0200

    kkkk

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Checkbox from '@mui/material/Checkbox';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-export default function Checkboxes() {
+export default function Checkbox() {
     return (
         <div>
             <Checkbox {...label} />
