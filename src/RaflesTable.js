b4db84871ad392aad40dd2a90c68d411c35011b9
Chavi6717 <a0504136711@gmail.com>
2022-01-23 15:33:47 +0200

    Add a RafflesTable component

---
+++ src/RaflesTable.js
@@ -0,0 +1,34 @@
+import { Button, TableContainer } from '@mui/material';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+export default function RaflesTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="center">הגרל</TableCell>
+                        <TableCell align="center">פריט</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <Button variant="outlined" >!הגרל</Button>
+                        {/* <TableCell align="center">פריט</TableCell> */}
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
