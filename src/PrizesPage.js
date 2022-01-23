a874e4fc111f01a505804cbfe53a1719630be232
racheliNaaman <racheli8014@gmail.com>
2022-01-23 11:56:34 +0200

    feat(PrizePage):Notice Modal - handle Close

--- src/PrizesPage.js
+++ src/PrizesPage.js
@@ -2,14 +2,20 @@ import ContinueToPayment from './ContinueToPayment';
 import TitleAppBar from './TitleAppBar';
 import Prizes from './Prizes';
 import NoticeModal from './NoticeModal';
+import Modal from '@mui/material/Modal';
+import { useState } from 'react';
 
 export default function PrizesPage() {
+    function onCloseModal() {
+        shouldShowNoticeModal = useState(false)
+    }
     return (
         <>
-            <TitleAppBar/>
-            <Prizes/>
-            <ContinueToPayment/>
-            <NoticeModal/>
+            <TitleAppBar />
+            <Prizes />
+            <ContinueToPayment />
+            <NoticeModal />
+            <Modal onClose={onCloseModal} />
         </>
     );
 }
