21b9934e151df21db36c69bdbcaf1f5c30e906b8
Chavi6717 <a0504136711@gmail.com>
2022-01-23 16:22:02 +0200

    fix: remove empty lines

--- src/RaflesTable.js
+++ src/RaflesTable.js
@@ -1,14 +1,11 @@
 import { Button, TableContainer } from '@mui/material';
-import * as React from 'react';
+import React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-
 
 export default function RaflesTable() {
     return (
