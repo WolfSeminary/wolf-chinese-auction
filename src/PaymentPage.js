8b40584b17139a09afc221b3420331f6239b15f5
shoshi4269 <sh6704269@gmail.com>
2022-01-25 17:46:03 +0200

    pass information of chosen prizes to payment page

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -4,7 +4,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ThankYouModal from './ThankYouModal'
 import { useNavigate } from 'react-router-dom';
 
-export default function PaymentPage() {
+export default function PaymentPage(prizes) {
   const [shouldShowThankYouModal, setShouldShowThankYouModal] = useState(false)
   function onCloseModal() {
     setShouldShowThankYouModal(false)
