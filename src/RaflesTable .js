250eecdd3bfe43a9b0aedc064b0b30af2320409d
chayale5281 <96553409+chayale5281@users.noreply.github.com>
2022-01-25 17:09:31 +0200

    feat(ruffle button):logic ruffle button update winner (#70)
    * logic ruffle button update winner

---
+++ src/RaflesTable .js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Winner from "./Winner";
+export default () => {
+    const [curentRaffle, setcurentRaffle] = useState({
+        userName: '',
+        prizeName: '----'
+    });
+
+    const onMakeRaffleClick = () => {
+        const listUsers = [];
+        listUsers = localStorage.getItem("listUsers");
+        var rand = Math.floor(Math.random() * listUsers.length()) + 1;
+        setcurentRaffle({
+            prizeName: localStorage.getItem("prizeName"),
+            userName: listUsers[rand]
+        })
+    }
+    return (
+        <>
+        </>
+    )
+}
